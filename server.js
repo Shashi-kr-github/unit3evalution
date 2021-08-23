78a20cc9f88aff04dc942cb76c8dee7b99df354e
Shashijass <shashi16ic14@gmail.com>
2021-08-23 16:30:28 +0530

    .

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 
 const app = express();
 
-app.use(expres.json());
+app.use(express.json());
 
 const connect = () =>{
     return mongoose.connect("mongodb://127.0.0.1:27017/Ecomerce1" , {
@@ -20,18 +20,21 @@ const productSchema = new mongoose.Schema({
     color_1: String,
     color_2: String,
     color_3: String,
+    men_: Boolean,
+    women_: Boolean,
 });
 
 const Item = mongoose.model("item" , productSchema)
 
-// app.post("/items", async function (req,res){
-//     await
-// })
+app.post("/items", async function (res,req){
+     const item = await Item.create(req.body);
+     return res.send(item);
+})
 // app.get("/items", async function (req,res){
 //     await 
 // })
 
-app.listen(13002, async () =>{
+app.listen(12002, async () =>{
      await connect();
     console.log("listen the port 3002");
 
