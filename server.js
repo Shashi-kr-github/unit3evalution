0379749ebfba257ca2dc02df37e16a09491ca799
Shashijass <shashi16ic14@gmail.com>
2021-08-23 15:50:26 +0530

    .

--- server.js
+++ server.js
@@ -1,5 +1,30 @@
+const express = require('express');
+
+const mongoose = require('mongoose');
+
 const app = express();
 
-app.listen(3002, () =>{
-    console.log("listen the port 3002")
-})
+const connect = () =>{
+    return mongoose.connect("mongodb://127.0.0.1:27017/Ecomerce1" , {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+         useCreateIndex: true,
+    })
+}
+
+const productSchema = new mongoose.Schema({
+    product_catogary: String,
+    price: Number,
+    color_1: String,
+    color_2: String,
+    color_3: String,
+});
+
+const Item = mongoose.model("item" , productSchema)
+
+
+
+app.listen(13002, () =>{
+    console.log("listen the port 3002");
+
+});
