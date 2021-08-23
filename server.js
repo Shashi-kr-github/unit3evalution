e88e6588070d695cbe85e6b1b1ef187c2aa3a9aa
Shashijass <shashi16ic14@gmail.com>
2021-08-23 17:00:31 +0530

    .

--- server.js
+++ server.js
@@ -15,24 +15,27 @@ const connect = () =>{
 }
 
 const productSchema = new mongoose.Schema({
-    product_catogary: String,
+    product_c: String,
     price: Number,
     color_1: String,
     color_2: String,
     color_3: String,
     men_: Boolean,
     women_: Boolean,
+},{versionKey: false
 });
 
 const Item = mongoose.model("item" , productSchema)
 
-app.post("/items", async function (res,req){
+app.post("/items", async function (req,res){
      const item = await Item.create(req.body);
      return res.send(item);
 })
-// app.get("/items", async function (req,res){
-//     await 
-// })
+
+app.get("/items", async function(req,res){
+    const items = await Item.find().lean().exec()
+    return res.send(items)
+})
 
 app.listen(12002, async () =>{
      await connect();
