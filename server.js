27d013513c03ce11e084c21bfefe6026210f9a84
Shashijass <shashi16ic14@gmail.com>
2021-08-23 17:45:37 +0530

    .

--- server.js
+++ server.js
@@ -14,18 +14,60 @@ const connect = () =>{
     })
 }
 
+const catgSchema = new mongoose.Schema(
+    {
+   name : {
+       type : String,
+       required: true
+   }
+
+   }, {
+       versionKey: false
+   }
+)
+
+const Catogery = mongoose.model("catogery" ,catgSchema )
+
+const colorSchema = new mongoose.Schema(
+    {
+   name : {
+       type : String,
+       required: true
+   }
+
+   }, {
+       versionKey: false
+   }
+)
+
+const Catogery = mongoose.model("color" ,colorSchema )
+
+
+
+
 const productSchema = new mongoose.Schema({
-    product_c: String,
-    price: Number,
-    color_1: String,
-    color_2: String,
-    color_3: String,
-    men_: Boolean,
-    women_: Boolean,
-},{versionKey: false
-});
+    product: {
+        type: String,
+        required: true
+    },
+    catogery : [
+        {
+           type: mongoose.Schema.Types.ObjectId,
+           ref: "catogery",
+           required: true
+        }
+    ]
+
+    },
+    {
+        versionKey: false
+    }
+)
+
 
 const Item = mongoose.model("item" , productSchema)
+
+const
 
 app.post("/items", async function (req,res){
      const item = await Item.create(req.body);
@@ -33,9 +75,17 @@ app.post("/items", async function (req,res){
 })
 
 app.get("/items", async function(req,res){
+   
     const items = await Item.find().lean().exec()
     return res.send(items)
 })
+
+// app.get("/items/:price", async function(req,res){
+//      console.log(req.params);
+//     const items = await Item.fi(req.prams.price).lean().exec();
+//     res.send(items)
+// })
+
 
 app.listen(12002, async () =>{
      await connect();
